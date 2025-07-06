ae311b838e386f2169e45f1c3a80142baac44b13
BHISHMA <bhishmadandekar@gmail.com>
2025-07-06 21:15:44 +0530

    Updated code

--- src/pages/Homepage/components/Skills.tsx
+++ src/pages/Homepage/components/Skills.tsx
@@ -5,7 +5,7 @@ function Skills() {
         <h1 className="text-5xl lg:text-6xl font-bold mb-[20px] text-white relative mx-[20px] md:mx-[100px]">
           Skills
         </h1>
-        <video src="public/v3.mp4" muted autoPlay loop className="absolute w-full h-full object-cover " preload="none"></video>
+        <video src="/v3.mp4" muted autoPlay loop className="absolute w-full h-full object-cover " preload="none"></video>
         <div className="grid grid-row-2 sm:grid-row-3 md:grid-cols-6 md:gap-7 items-center relative p-[20px] md:p-[100px]">
           <div className="col-span-2 bg-none hover:bg-[#006EBB] transition duration-500 delay-55 ease-in-out p-2 rounded-3xl "><img src="docker.png" className="w-full h-[81px] md:h-[120px] object-contain bg-white rounded-2xl" alt="" /></div>
           <div className="bg-none hover:bg-[#00D9FD] transition duration-500 delay-75 ease-in-out p-2 rounded-3xl "><img src="react.jpg" className="w-full h-[81px] md:h-[120px] object-contain bg-white rounded-2xl" alt="" /></div>
